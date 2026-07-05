a02733983744dfa86211629fd39a6c8d42b787a0
Amata Utama <amata.utama1@lotuss.com>
2026-07-05 13:01:52 +0700

    chore: update next.config.ts configuration settings

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,14 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production" || process.env.GITHUB_ACTIONS === "true";
+const isProd =
+  process.env.NODE_ENV === "production" ||
+  process.env.GITHUB_ACTIONS === "true";
 
 const nextConfig: NextConfig = {
   output: "export",
   basePath: isProd ? "/grade1-math-quiz" : "",
-  trailingSlash: true, // บังคับให้ใช้ trailing slash เพื่อแก้ไขปัญหาการอ้างอิง path ใน GitHub Pages
+  assetPrefix: isProd ? "/grade1-math-quiz/" : "",
+  // trailingSlash: true, // บังคับให้ใช้ trailing slash เพื่อแก้ไขปัญหาการอ้างอิง path ใน GitHub Pages
   images: {
     unoptimized: true,
   },
